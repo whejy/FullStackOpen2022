727c3a929f6cf1b8a466d2df6e422c12bfed1983
whejy <john.wheldale@outlook.com>
2022-08-26 16:16:40 +0930

    add middleware

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,18 +1,23 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
 
-blogsRouter.get('/', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
+blogsRouter.get('/', (request, response, next) => {
+  Blog.find({})
+    .then((blogs) => {
+      response.json(blogs);
+    })
+    .catch((error) => next(error));
 });
 
 blogsRouter.post('/', (request, response) => {
   const blog = new Blog(request.body);
 
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
+  blog
+    .save()
+    .then((result) => {
+      response.status(201).json(result);
+    })
+    .catch((error) => next(error));
 });
 
 module.exports = blogsRouter;
