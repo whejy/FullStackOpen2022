b829c88f9c8600439c52bdeb2e67ba9e7341f4b0
whejy <john.wheldale@outlook.com>
2022-09-05 15:24:55 +0930

    4.19 add token auth for post requests

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,6 +1,15 @@
+const jwt = require('jsonwebtoken');
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
 const User = require('../models/user');
+
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization');
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7);
+  }
+  return null;
+};
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { blogs: 0 });
@@ -39,6 +48,14 @@ blogsRouter.delete('/:id', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body;
+  const token = getTokenFrom(request);
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' });
+  }
+
+  const user = await User.findById(decodedToken.id);
 
   if (!body.likes) {
     body.likes = 0;
@@ -47,8 +64,6 @@ blogsRouter.post('/', async (request, response) => {
   if (!body.title || !body.url) {
     return response.status(400).end();
   }
-
-  const user = await User.findOne({});
 
   const blog = new Blog({
     title: body.title,
