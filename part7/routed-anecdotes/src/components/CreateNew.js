29ff58a39e0e2da74bb9e801643dbc7c13b760bc
whejy <john.wheldale@outlook.com>
2022-09-29 15:17:12 +0930

    7.4-7.6 custom hooks

--- part7/routed-anecdotes/src/components/CreateNew.js
+++ part7/routed-anecdotes/src/components/CreateNew.js
@@ -1,24 +1,34 @@
-import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { useField } from '../hooks'
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const { reset: resetContent, ...content } = useField('text')
+  const { reset: resetAuthor, ...author } = useField('text')
+  const { reset: resetInfo, ...info } = useField('text')
 
   let navigate = useNavigate()
+
+  const handleReset = (e) => {
+    e.preventDefault()
+    resetContent()
+    resetAuthor()
+    resetInfo()
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0,
     })
 
     navigate('/')
-    props.notify(`New Anecdote Created - ${content} by ${author}`, 5)
+    props.notify(
+      `New Anecdote Created - ${content.value} by ${author.value}`,
+      5
+    )
   }
 
   return (
@@ -27,29 +37,18 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input
-            name="content"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
+          <input {...content} name="content" />
         </div>
         <div>
           author
-          <input
-            name="author"
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-          />
+          <input {...author} name="author" />
         </div>
         <div>
           url for more info
-          <input
-            name="info"
-            value={info}
-            onChange={(e) => setInfo(e.target.value)}
-          />
+          <input {...info} name="info" />
         </div>
-        <button>create</button>
+        <button type="submit">Create</button>
+        <button onClick={handleReset}>Clear</button>
       </form>
     </div>
   )
