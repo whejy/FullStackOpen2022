298e106bca3bceff9f2cc19e5c3a202ff04a59f8
whejy <john.wheldale@outlook.com>
2022-10-21 16:44:52 +1030

    add notifications

--- part8/library-frontend/src/components/EditAuthor.js
+++ part8/library-frontend/src/components/EditAuthor.js
@@ -1,12 +1,14 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import Notification from './Notification'
 import { useMutation } from '@apollo/client'
 import { EDIT_AUTHOR, ALL_AUTHORS } from '../queries'
 
 const EditAuthor = () => {
   const [name, setName] = useState('')
   const [born, setBorn] = useState('')
+  const [notification, setNotification] = useState('')
 
-  const [updateAuthor] = useMutation(EDIT_AUTHOR, {
+  const [updateAuthor, result] = useMutation(EDIT_AUTHOR, {
     refetchQueries: [{ query: ALL_AUTHORS }],
   })
 
@@ -20,10 +22,29 @@ const EditAuthor = () => {
     setName('')
     setBorn('')
   }
+
+  useEffect(() => {
+    if (result.data) {
+      if (result.data.editAuthor === null) {
+        notify('Cannot Find User', 'error')
+      } else {
+        notify('Successfully Updated User')
+      }
+    }
+  }, [result.data])
+
+  const notify = (message, type = 'success') => {
+    setNotification({ message, type })
+    setTimeout(() => {
+      setNotification(null)
+    }, 3000)
+  }
+
   return (
     <div>
+      <h2>set birthyear</h2>
+      <Notification notification={notification} />
       <form onSubmit={submit}>
-        <h2>set birthyear</h2>
         <div>
           name:
           <input
