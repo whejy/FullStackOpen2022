264033ef36f70875f19cc3172a9fc0d12fa1b7a7
whejy <john.wheldale@outlook.com>
2022-10-25 13:59:37 +1030

    8.15 add DB validation error handling

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -1,4 +1,4 @@
-const { ApolloServer, gql } = require('apollo-server')
+const { ApolloServer, UserInputError, gql } = require('apollo-server')
 const mongoose = require('mongoose')
 const Book = require('./models/book')
 const Author = require('./models/author')
@@ -83,11 +83,24 @@ const resolvers = {
 
       if (!author) {
         author = new Author({ name: args.author })
-        await author.save()
+        try {
+          await author.save()
+        } catch (error) {
+          throw new UserInputError(error.message, {
+            invalidArgs: args,
+          })
+        }
       }
 
       const book = new Book({ ...args, author: author.id })
-      await book.save()
+
+      try {
+        await book.save()
+      } catch (error) {
+        throw new UserInputError(error.message, {
+          invalidArgs: args,
+        })
+      }
       return book.populate('author')
     },
     editAuthor: async (root, args) => {
@@ -98,7 +111,14 @@ const resolvers = {
       }
 
       author.born = args.born
-      await author.save()
+
+      try {
+        await author.save()
+      } catch (error) {
+        throw new UserInputError(error.message, {
+          invalidArgs: args,
+        })
+      }
       return author
     },
   },
