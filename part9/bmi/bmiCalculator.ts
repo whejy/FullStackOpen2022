003c99c0f42718040b2179ce0ad4c048cbd45a8c
whejy <john.wheldale@outlook.com>
2022-12-05 16:05:27 +1030

    9.1 bmi

---
+++ part9/bmi/bmiCalculator.ts
@@ -0,0 +1,12 @@
+function calculateBmi(height: number, weight: number): string {
+    const bmi = weight / (height/ 100)**2
+
+    if (bmi <= 18.4) {
+        return "Underweight";
+    } else if (bmi >= 25) {
+        return "Overweight"
+    }
+    return "Normal (healthy weight)"
+}
+
+console.log(calculateBmi(182, 74))
