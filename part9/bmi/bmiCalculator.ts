5fdad8e4c34e721727a4256b53d9fba9df4ea6ca
whejy <john.wheldale@outlook.com>
2022-12-06 15:15:32 +1030

    9.3 allow command line input

--- part9/bmi/bmiCalculator.ts
+++ part9/bmi/bmiCalculator.ts
@@ -1,3 +1,22 @@
+interface Values {
+    height: number;
+    weight: number;
+}
+
+function parseArguments (args: Array<string>): Values {
+    if (args.length < 4) throw new Error('Not enough arguments');
+    if (args.length > 4) throw new Error('Too many arguments')
+
+    if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+        return {
+            height: Number(args[2]),
+            weight: Number(args[3])
+        }
+    } else {
+        throw new Error('Provided values are not numbers.')
+    }
+}
+
 function calculateBmi(height: number, weight: number): string {
     const bmi = weight / (height/ 100)**2;
 
@@ -9,4 +28,13 @@ function calculateBmi(height: number, weight: number): string {
     return "Normal (healthy weight)";
 };
 
-console.log(calculateBmi(182, 74));
+try {
+    const {height, weight} = parseArguments(process.argv);
+    console.log(calculateBmi(height, weight))
+} catch (error: unknown) {
+    let errorMessage = 'Something went wrong.'
+    if (error instanceof Error) {
+        errorMessage += ' Error: ' + error.message
+    }
+    console.log(errorMessage)
+}
--- part9/bmi/exerciseCalculator.ts
+++ part9/bmi/exerciseCalculator.ts
@@ -1,3 +1,5 @@
+const prompt = require('prompt-sync')()
+
 interface Result {
     periodLength: number;
     trainingDays: number;
@@ -8,7 +10,53 @@ interface Result {
     average: number;
 }
 
-function calculateExercises(hours: Array<number>, target: number): Result {
+interface rawValues {
+    rawTarget: string;
+    rawHours: Array<string>;
+}
+
+interface parsedValues {
+    target: number;
+    dailyHours: Array<number>;
+}
+
+function getInput(): rawValues {
+    let rawHours = [];
+    const rawTarget = prompt('What is your target daily hours? ');
+
+    for (let i = 1; i < 100; i++) {
+        const input = prompt(`How many hours did you exercise on day ${i}? (Press 'enter' to exit): `);
+
+        if (input) {
+            rawHours.push(input);
+        } else {
+            break;
+        }
+    }
+    return {rawTarget, rawHours}
+}
+
+function parseArguments(rawTarget: string, rawHours: Array<string>): parsedValues {
+    if (Number(rawTarget) <= 0) {
+        throw new Error('Target hours must be greater than zero.')
+    }
+
+    if (rawHours.length < 1) {
+        throw new Error('Please enter at least one value for days exercised.')
+    }
+
+    if (!isNaN(Number(rawTarget)) && !rawHours.some(el => isNaN(Number(el)))) {
+        return {
+            target: Number(rawTarget),
+            dailyHours: rawHours.map(el => Number(el))
+        }
+    } else {
+        throw new Error('Provided values are not numbers.')
+    }
+}
+
+
+function calculateExercises(target: number, hours: Array<number>): Result {
     const periodLength = hours.length;
     const trainingDays = hours.filter(x => x > 0).length;
     const success = hours.every(x => x >= target);
@@ -18,7 +66,7 @@ function calculateExercises(hours: Array<number>, target: number): Result {
 
     let rating = 3;
     let ratingDescription = "Well done!";
-    
+
     if (percentage < 0.5) {
         rating = 0;
         ratingDescription = "There's always next week!";
@@ -41,4 +89,16 @@ function calculateExercises(hours: Array<number>, target: number): Result {
     }
 }
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
+try {
+    const {rawTarget, rawHours} = getInput()
+    const {target, dailyHours} = parseArguments(rawTarget, rawHours)
+    console.log(calculateExercises(target, dailyHours))
+} catch (error: unknown) {
+    let errorMessage = 'Something went wrong.'
+    if (error instanceof Error) {
+        errorMessage += ' Error: ' + error.message
+    }
+    console.log(errorMessage)
+}
+
+export {}
